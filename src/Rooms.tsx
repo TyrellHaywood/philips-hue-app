b199c9b7bb6bcb1610580586d0ec6940e0e2361e
TyrellHaywood <tyrelltman@gmail.com>
2023-10-07 17:08:43 -0400

    scroll bar added

--- src/Rooms.tsx
+++ src/Rooms.tsx
@@ -11,31 +11,40 @@ const Rooms = () => {
       <div className="rooms-title">
         <h2 className="rooms-title__title">Rooms</h2>
       </div>
-
-      <div className="room-component">
-        <div className="room-component__information">
-          <img className="room-component__information__img"></img>
-          <div className="room-component__information__title">
-            <h1 className="room-component__information__title__title">
-              Tyrell's Room
-            </h1>
-            <h2 className="room-component__information__title__subtitle">
-              lights are on
-            </h2>
+      <div className="room-section">
+        {/*  */}
+        <div className="room-component">
+          {/* room info */}
+          <div className="room-component__information">
+            <img className="room-component__information__img"></img>
+            <div className="room-component__information__title">
+              <h1 className="room-component__information__title__title">
+                Tyrell's Room
+              </h1>
+              <h2 className="room-component__information__title__subtitle">
+                lights are on
+              </h2>
+            </div>
+            <div className="room-component__information__button">
+              <div className="room-component__information__button__circle"></div>
+            </div>
           </div>
-          <div className="room-component__information__button">
-            <div className="room-component__information__button__circle"></div>
-          </div>
-        </div>
-
-        <div className="room-component__brightness">
-          <div className="room-component__brightness__slider">
-            <div className="room-component__brightness__slider__inner">
-              <div className="room-component__brightness__slider__inner__circle"></div>
+          {/* room brightness */}
+          <div className="room-component__brightness">
+            <div className="room-component__brightness__slider">
+              <div className="room-component__brightness__slider__inner">
+                <div className="room-component__brightness__slider__inner__circle"></div>
+              </div>
             </div>
           </div>
         </div>
+        <div className="room-scrollbar">
+          <div className="room-scrollbar__bar">
+            <div className="room-scrollbar__bar__cursor"></div>
+          </div>
+        </div>
       </div>
+      {/*  */}
     </div>
   );
 };
