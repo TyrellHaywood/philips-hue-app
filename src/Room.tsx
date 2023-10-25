851c78a286b11eee024cc553030475735ca36c10
TyrellHaywood <tyrelltman@gmail.com>
2023-10-24 22:31:26 -0400

    separted room into specific component for future

---
+++ src/Room.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Rooms.scss";
+
+const Room = () => {
+  return (
+    <div className="room-component">
+      {/* room info */}
+      <div className="room-component__information">
+        <img className="room-component__information__img"></img>
+        <div className="room-component__information__title">
+          <h1 className="room-component__information__title__title">
+            Tyrell's Room
+          </h1>
+          <h2 className="room-component__information__title__subtitle">
+            lights are on
+          </h2>
+        </div>
+        <div className="room-component__information__button">
+          <div className="room-component__information__button__circle"></div>
+        </div>
+      </div>
+      {/* room brightness */}
+      <div className="room-component__brightness">
+        <div className="room-component__brightness__slider">
+          <div className="room-component__brightness__slider__inner">
+            <div className="room-component__brightness__slider__inner__circle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Room;
--- src/Rooms.tsx
+++ src/Rooms.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Rooms.scss";
+import Room from "./Room";
 
 const Rooms = () => {
   return (
@@ -13,31 +14,7 @@ const Rooms = () => {
       </div>
       <div className="room-section">
         {/*  */}
-        <div className="room-component">
-          {/* room info */}
-          <div className="room-component__information">
-            <img className="room-component__information__img"></img>
-            <div className="room-component__information__title">
-              <h1 className="room-component__information__title__title">
-                Tyrell's Room
-              </h1>
-              <h2 className="room-component__information__title__subtitle">
-                lights are on
-              </h2>
-            </div>
-            <div className="room-component__information__button">
-              <div className="room-component__information__button__circle"></div>
-            </div>
-          </div>
-          {/* room brightness */}
-          <div className="room-component__brightness">
-            <div className="room-component__brightness__slider">
-              <div className="room-component__brightness__slider__inner">
-                <div className="room-component__brightness__slider__inner__circle"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Room />
         <div className="room-scrollbar">
           <div className="room-scrollbar__bar">
             <div className="room-scrollbar__bar__cursor"></div>
