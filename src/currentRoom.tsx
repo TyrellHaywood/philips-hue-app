21b03bfe6774ba78def69b1ae294dda6d283f677
TyrellHaywood <tyrelltman@gmail.com>
2023-11-08 23:17:31 -0500

    centering info container

--- src/currentRoom.tsx
+++ src/currentRoom.tsx
@@ -1,14 +1,49 @@
 import React from "react";
 import "./CurrentRoom.scss"; // imports stylesheet;
-
 import { useState, useEffect } from "react";
 
-const currentRoom = () => {
+//define type for props (avoid typescript errors)
+interface CurrentRoomProps {
+  toggleLightsPower: () => void;
+  adjustLightsBrightness: (brightness: number) => void;
+}
+
+const CurrentRoom: React.FC<CurrentRoomProps> = ({
+  toggleLightsPower,
+  adjustLightsBrightness,
+}) => {
+  const [isLightOn, setLightOn] = useState(false);
+  const [brightness, setBrightness] = useState(0); // Default brightness value
+
+  const handleToggleLights = () => {
+    setLightOn(!isLightOn);
+    toggleLightsPower(); //call function from the prop
+  };
+
   return (
     <div className="current-room">
-      <h1>MYROOM</h1>
+      <header className="current-room-header">
+        <div className="current-room-header__top">
+          <div className="current-room-header__top__round-button back-button">
+            <button>back</button>
+          </div>
+          <h1>Tyrell's Room</h1>
+          <div className="current-room-header__top__round-button edit-button">
+            <button>edit</button>
+          </div>
+          <div className="current-room-header__top__light-button">
+            <button
+              className={`current-room-header__top__light-button__circle ${
+                isLightOn ? "on" : "off"
+              }`}
+              onClick={handleToggleLights}
+            ></button>
+          </div>
+        </div>
+        <div className="current-room-header__bottom"></div>
+      </header>
     </div>
   );
 };
 
-export default currentRoom;
+export default CurrentRoom;
