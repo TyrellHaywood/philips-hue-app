477009902e80eea42b086e8209de9814c221e9d2
TyrellHaywood <tyrelltman@gmail.com>
2024-01-24 17:09:11 -0500

    fixing formatting of currentRoom for future

--- src/currentRoom.tsx
+++ src/currentRoom.tsx
@@ -83,115 +83,119 @@ const CurrentRoom: React.FC<CurrentRoomProps> = ({
 
   return (
     <div className="current-room">
-      <header className="current-room-header">
-        <div
-          className={`current-room-header__top ${
-            isLightOn ? "lights-on" : "lights-off"
-          }`}
-        >
-          <div className="current-room-header__top__round-button back-button">
-            <button
-              className="back-button__arrow-button"
-              onClick={handleBackClick}
-            >
-              <span className="material-icons md-light md-36">arrow_back</span>
-            </button>
+      <div className="current-room-main">
+        <header className="current-room-main__header">
+          <div
+            className={`current-room-main__header__top ${
+              isLightOn ? "lights-on" : "lights-off"
+            }`}
+          >
+            <div className="current-room-main__header__top__round-button back-button">
+              <button
+                className="back-button__arrow-button"
+                onClick={handleBackClick}
+              >
+                <span className="material-icons md-light md-36">
+                  arrow_back
+                </span>
+              </button>
+            </div>
+            <h1>Tyrell's Room</h1>
+            <div className="current-room-main__header__top__round-button edit-button">
+              <button className="btn">
+                <div className="edit-button__more-options"></div>
+                <div className="edit-button__more-options"></div>
+                <div className="edit-button__more-options"></div>
+              </button>
+            </div>
+            <div className="current-room-main__header__top__light-button">
+              <button
+                className={`current-room-main__header__top__light-button__circle ${
+                  isLightOn ? "on" : "off"
+                }`}
+                onClick={handleToggleLights}
+              ></button>
+            </div>
           </div>
-          <h1>Tyrell's Room</h1>
-          <div className="current-room-header__top__round-button edit-button">
-            <button className="btn">
-              <div className="edit-button__more-options"></div>
-              <div className="edit-button__more-options"></div>
-              <div className="edit-button__more-options"></div>
-            </button>
-          </div>
-          <div className="current-room-header__top__light-button">
-            <button
-              className={`current-room-header__top__light-button__circle ${
-                isLightOn ? "on" : "off"
-              }`}
-              onClick={handleToggleLights}
-            ></button>
-          </div>
-        </div>
-        <div className="current-room-header__bottom">
-          <div className="current-room-header__bottom__brightness-slider">
-            <input
-              type="range"
-              min="0"
-              max="255" // adjust the max value based on light's maximum brightness
-              value={brightness}
-              onChange={handleBrightnessChange}
-              className="current-room-header__bottom__brightness-slider__input"
-              style={
-                { "--slider-width": `${sliderWidth}%` } as React.CSSProperties
-              }
-            />
-          </div>
-        </div>
-      </header>
-      <div className="current-room-content">
-        <div className="current-room-content__mid-header">
-          <p>MY SCENES</p>
-          <div className="current-room-content__mid-header__search-bar">
-            <button
-              className="current-room-content__mid-header__search-bar__button"
-              onClick={(event) => handleSearchScene(event, searchValue)}
-            >
-              <span className="material-icons md-light md-36">search</span>
-            </button>
-            <input
-              type="text"
-              placeholder="Search for your scene"
-              value={searchValue}
-              onChange={handleSearchInput}
-              onKeyDown={handleSearchKeyPress}
-            />
-          </div>
-          <div className="current-room-content__mid-header__new-scene">
-            <button>
-              <span className="material-icons md-light md-36">add</span>
-            </button>
-          </div>
-        </div>
-        <div className="current-room-content__scenes">
-          {Array.isArray(scenesData) &&
-            scenesData.map((scene) => {
-              const isSelected =
-                selectedScene && (selectedScene as any).id === scene.id;
-              // console.log(`Scene ${scene.id} is selected: ${isSelected}`);
-              return isSelected ? (
-                <SceneTarget
-                  key={scene.id}
-                  sceneId={scene.id}
-                  sceneName={scene.name}
-                  handleSelectScene={handleSelectScene}
-                />
-              ) : (
-                <Scene
-                  key={scene.id}
-                  sceneId={scene.id}
-                  sceneName={scene.name}
-                  handleSelectScene={handleSelectScene}
-                />
-              );
-            })}
-        </div>
-        <div className="current-room-content__lights-title">
-          <p>LIGHTS</p>
-        </div>
-        <div className="current-room-content__lights">
-          {Array.isArray(lightsData) &&
-            lightsData.map((light) => (
-              <Light
-                lightId={light.id}
-                lightName={light.name}
-                lightCT={light.ct}
-                lightXY={light.xy}
-                lightBri={light.bri}
-                toggleSingleLightPower={toggleSingleLightPower}
+          <div className="current-room-main__header__bottom">
+            <div className="current-room-main__header__bottom__brightness-slider">
+              <input
+                type="range"
+                min="0"
+                max="255" // adjust the max value based on light's maximum brightness
+                value={brightness}
+                onChange={handleBrightnessChange}
+                className="current-room-main__header__bottom__brightness-slider__input"
+                style={
+                  { "--slider-width": `${sliderWidth}%` } as React.CSSProperties
+                }
               />
-            ))}
+            </div>
+          </div>
+        </header>
+        <div className="current-room-content">
+          <div className="current-room-content__mid-header">
+            <p>MY SCENES</p>
+            <div className="current-room-content__mid-header__search-bar">
+              <button
+                className="current-room-content__mid-header__search-bar__button"
+                onClick={(event) => handleSearchScene(event, searchValue)}
+              >
+                <span className="material-icons md-light md-36">search</span>
+              </button>
+              <input
+                type="text"
+                placeholder="Search for your scene"
+                value={searchValue}
+                onChange={handleSearchInput}
+                onKeyDown={handleSearchKeyPress}
+              />
+            </div>
+            <div className="current-room-content__mid-header__new-scene">
+              <button>
+                <span className="material-icons md-light md-36">add</span>
+              </button>
+            </div>
+          </div>
+          <div className="current-room-content__scenes">
+            {Array.isArray(scenesData) &&
+              scenesData.map((scene) => {
+                const isSelected =
+                  selectedScene && (selectedScene as any).id === scene.id;
+                // console.log(`Scene ${scene.id} is selected: ${isSelected}`);
+                return isSelected ? (
+                  <SceneTarget
+                    key={scene.id}
+                    sceneId={scene.id}
+                    sceneName={scene.name}
+                    handleSelectScene={handleSelectScene}
+                  />
+                ) : (
+                  <Scene
+                    key={scene.id}
+                    sceneId={scene.id}
+                    sceneName={scene.name}
+                    handleSelectScene={handleSelectScene}
+                  />
+                );
+              })}
+          </div>
+          <div className="current-room-content__lights-title">
+            <p>LIGHTS</p>
+          </div>
+          <div className="current-room-content__lights">
+            {Array.isArray(lightsData) &&
+              lightsData.map((light) => (
+                <Light
+                  lightId={light.id}
+                  lightName={light.name}
+                  lightCT={light.ct}
+                  lightXY={light.xy}
+                  lightBri={light.bri}
+                  toggleSingleLightPower={toggleSingleLightPower}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </div>
